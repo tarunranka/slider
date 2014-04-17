b99fc3d908ac653eaa746be935c9e26228d0e2a5
tarunranka <tarunranka.m@gmail.com>
2014-04-17 03:15:50 -0700

    Add Drag and swipe

--- js/jquery.slider.js
+++ js/jquery.slider.js
@@ -38,7 +38,7 @@
 			var prev = $('.control_prev');
 			var page = $('.pagination li');
 			slider.clickFlag=0;
-			slider.current = 1;			
+			slider.current = 1;
 			var cloneSlide_1 = li.eq(slider.current).clone().addClass('clone');
 			var cloneSlide_last = li.eq((slider.itemsLength-1)).clone().addClass('clone');
 			ul.append(cloneSlide_1);
@@ -47,6 +47,11 @@
 			var currentli = li.eq(slider.current-1);
 			currentli.addClass('active');
 			page.eq(slider.current-1).addClass('active');
+			
+			var isMouseDown=false,
+				started =false,
+				drag={},
+				delta={};
 			$('.control_next').click(function(e){
 				e.preventDefault();
 			
@@ -66,6 +71,60 @@
 				}
 				return false;
 			});
+			li.on('mousedown', function(e) {
+                isMouseDown = true;
+				drag.x = e.pageX;
+				drag.y = e.pageY;
+				
+            });
+			li.on('mousemove', function(e) {			
+                if (isMouseDown) {					
+					console.log('stopped'+e.pageX +' '+drag.x);
+					delta.x = e.pageX;
+					delta.y = e.pageY;
+					var angle = calculateDirection(drag,delta);
+					console.log(angle);
+					//console.log('stopped'+delta.x+'y...  '+delta.y);
+					if(angle =="LEFT"){
+						console.log(delta.x);
+						if (slider.current  < (slider.itemsLength)) {
+						slider.current  += 1; 
+					} else {
+						slider.current  = 1; 
+					}
+					slideTo(slider.current,'next');
+					li.removeClass('active').eq(slider.current-1).addClass('active');
+					setTimeout(function(){
+						page.removeClass('active').eq(slider.current-1).addClass('active');
+					},100);
+					isMouseDown = false;					
+					} else if(angle =="RIGHT"){
+						console.log(delta.x);
+						if (slider.current  == 1) {
+						slider.current  = (slider.itemsLength); 
+					} else {
+						slider.current  -= 1; 
+					}
+					slideTo(slider.current,'prev');
+					li.removeClass('active').eq(slider.current-1).addClass('active');
+					setTimeout(function(){
+						page.removeClass('active').eq(slider.current-1).addClass('active');
+					},100);
+					isMouseDown = false;
+					} else {
+						isMouseDown = false;
+					}
+				}
+			});
+			li.on('mouseup mouseleave', function() {
+				
+                if (isMouseDown) {
+                    // dragging stopped
+					
+					isMouseDown = false;
+                }             
+            });
+			
 			$('.control_prev').click(function(e){
 				e.preventDefault();
 				if(slider.clickFlag == 0){
@@ -122,7 +181,34 @@
 					slider.clickFlag = 0;
 				});
 			}			
-		};		
+		};
+		var calculateAngle= function(startPoint, endPoint){
+			var x = startPoint.x - endPoint.x;
+            var y = endPoint.y - startPoint.y;
+            var r = Math.atan2(y, x); //radians
+            var angle = Math.round(r * 180 / Math.PI); //degrees
+
+            //ensure value is positive
+            if (angle < 0) {
+                angle = 360 - Math.abs(angle);
+            }
+			
+            return angle;
+		};
+		var calculateDirection=function(startPoint, endPoint) {
+            var angle = calculateAngle(startPoint, endPoint);
+            if ((angle <= 45) && (angle >= 0)) {
+                return "LEFT";
+            } else if ((angle <= 360) && (angle >= 315)) {
+                return "LEFT";
+            } else if ((angle >= 135) && (angle <= 225)) {
+                return "RIGHT";
+            } else if ((angle > 45) && (angle < 135)) {
+                return "DOWN";
+            } else {
+                return "UP";
+            }
+        }
 		init();
 		return this;
     
